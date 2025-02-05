4495229be4d22a24a87db62823a6b169945fe765
Bhaskar Singhal <singhalbhaskar@google.com>
2025-02-05 06:32:57 +0000

    Fix versions.tf

--- versions.tf
+++ versions.tf
@@ -16,4 +16,19 @@
 
 terraform {
   required_version = ">= 0.15.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.83.0, < 7"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.83.0, < 7"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-vpc/v0.0.1"
+  }
 }
